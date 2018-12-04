4eadd20d2c31b36fd2f74240b6edd6895ad98d13
np <npdev453@users.noreply.github.com>
2018-12-04 20:51:25 +0300

    c10

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ define((require, exports, module) => {
 
     let CommandManager = brackets.getModule("command/CommandManager")
     let KeyBindingManager = brackets.getModule("command/KeyBindingManager")
-
+c1
     let CopyToJsonArray = () => {
 
         let editor = brackets.getModule('editor/EditorManager').getCurrentFullEditor();
