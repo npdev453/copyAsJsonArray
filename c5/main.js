acb96790844960b857acb7e6cf1a1abf471ec685
np <npdev453@users.noreply.github.com>
2018-12-04 20:27:38 +0300

    c8

--- c5/main.js
+++ c5/main.js
@@ -25,7 +25,7 @@ define((require, exports, module) => {
         let input = document.createElement('input');
          input.style.position = 'fixed';
         input.style .opacity = 0;
-        input.value = text;
+          input.value = text;
         document.body.appendChild(input);
        input.select();
         document.execCommand('Copy');
